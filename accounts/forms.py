6a8926ab2f391063e18c34ed1754feefe4e69459
javad <javad.feyzabadi@yahoo.com>
2023-02-15 21:39:19 +0330

    create form

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,7 +15,7 @@ class UserCreationForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password1'] and cd['password2'] and cd['password1'] != cd['password2']:
-            raise ValidationError('Password Dont Match')
+            raise ValidationError('Passwords Dont Match')
         return cd['password2']
 
     def save(self,commit = True):
@@ -23,6 +23,7 @@ class UserCreationForm(forms.ModelForm):
         user.set_password(self.cleaned_data['password1'])
         if commit:
             user.save()
+        return user
 
     
 class UserChangeForm(forms.ModelForm):
