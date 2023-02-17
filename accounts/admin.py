06e0a1285ad085547ac1c1cef98b8398831797b9
javad <javad.feyzabadi@yahoo.com>
2023-02-17 21:31:37 +0330

    otp model

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,7 +3,15 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
 
 from . forms import UserChangeForm,UserCreationForm
-from . models import User
+from . models import User,OtpCode
+
+
+
+@admin.register(OtpCode)
+class OtpCodeAdmin(admin.ModelAdmin):
+    list_display = ('phone_number' , 'code' , 'created')
+
+
 
 
 class UserAdmin(BaseUserAdmin):
