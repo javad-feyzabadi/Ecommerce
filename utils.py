a24e9f104e304146fb58afee4fdb49d2e64ca6d1
javad <javad.feyzabadi@yahoo.com>
2023-02-19 02:14:36 +0330

    add kavenegar

--- utils.py
+++ utils.py
@@ -1,2 +1,16 @@
+from kavenegar import * 
+
 def send_otp_code(phone_number,code):
-    pass
+    try:
+        api = KavenegarAPI('7768697352574A4F56676A7342327A4650685748345050503331442B4D616B566C4D4576766B507A667A303D') 
+        params = {
+            'sender' : '',
+            'receptor': 'phone_number', 
+            'message' :f'{code} THis Is Code'
+        } 
+        response = api.sms_send(params) 
+        print(response)
+    except APIException as e: 
+            print(e)
+    except HTTPException as e: 
+            print(e)
