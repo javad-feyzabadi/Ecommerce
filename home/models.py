8c382fbebded50747149b9540cd0adf7bf91aa1d
javad <javad.feyzabadi@yahoo.com>
2023-02-19 17:11:54 +0330

    create models for shop

--- home/models.py
+++ home/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name='products')
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200,unique=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    price = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    avilable = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.name
+        
+
